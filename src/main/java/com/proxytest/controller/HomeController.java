5399f1f2013d6be2714507b5e4357732a4d0804e
potatoCompletion <033kws2628@gmail.com>
2022-11-21 15:16:46 +0900

    WebClient 추가 (Bean 등록, Autowired)

--- src/main/java/com/proxytest/controller/HomeController.java
+++ src/main/java/com/proxytest/controller/HomeController.java
@@ -1,22 +1,13 @@
 package com.proxytest.controller;
 
 import com.proxytest.service.HomeService;
-import lombok.val;
-import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -24,10 +15,12 @@ public class HomeController {
     private final HomeService homeService;
 
     @Autowired
-    public HomeController(HomeService homeService) { this.homeService = homeService; }
+    public HomeController(HomeService homeService) {
+        this.homeService = homeService;
+    }
 
     @GetMapping("/")
-    public String home(@RequestParam("url")String url, Model model) {
+    public String home(@RequestParam("url") String url, Model model) {
         model.addAttribute("url", url);
         model.addAttribute("request", "리퀘스트 전송완료!");
         model.addAttribute("response", "리스폰스 전송완료!");
@@ -35,14 +28,16 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping(value = "/프록시", method = { RequestMethod.GET, RequestMethod.POST })
-    public String getRequestValue(HttpServletRequest request, @RequestParam("url") String url, @RequestBody String postData, Model model) throws IOException{
+    @RequestMapping(value = "/프록시", method = {RequestMethod.GET, RequestMethod.POST})
+    public String getRequestValue(HttpServletRequest request, @RequestParam("url") String url, @RequestBody String postData, Model model) throws IOException {
         var headerData = homeService.getHeader(request);
         StringBuilder requestData = new StringBuilder(headerData);
         requestData.append(System.getProperty("line.separator"));
         requestData.append(postData);
 
         String nlString = System.getProperty("line.separator");
+
+        var temp = homeService.sendRequest("localhost:8080/프록시?url=kimwansu");
 
         model.addAttribute("url", url);
         model.addAttribute("request", requestData);
--- src/main/java/com/proxytest/service/HomeService.java
+++ src/main/java/com/proxytest/service/HomeService.java
@@ -1,6 +1,10 @@
 package com.proxytest.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -8,16 +12,23 @@ import java.util.HashMap;
 @Service
 public class HomeService {
 
-    public String getHeader(HttpServletRequest request) {
-        var headerNames = request.getHeaderNames();
+    private final WebClient webClient;
 
-        HashMap<String, String> headers = new HashMap<>();
+    @Autowired
+    public HomeService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public String getHeader(HttpServletRequest request) {
+        var headerNames = request.getHeaderNames(); // 헤더 키 값 가져오기
+
+        HashMap<String, String> headers = new HashMap<>();  // 키 값에 밸류값 대입해서 해시맵으로 추출
         while(headerNames.hasMoreElements()) {
             String key = headerNames.nextElement();
             headers.put(key, request.getHeader(key));
         }
 
-        StringBuilder headerData = new StringBuilder();
+        StringBuilder headerData = new StringBuilder(); // 저장한 해시맵 -> 스트링으로 변경
         for (String nowKey : headers.keySet()) {
             headerData.append(nowKey);
             headerData.append(": ");
@@ -25,6 +36,15 @@ public class HomeService {
         }
 
         return headerData.toString();
+    }
+
+    public Mono<String> sendRequest(String url) {
+        return webClient.mutate()
+                .build()
+                .post()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class);
     }
 
 }
